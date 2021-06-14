743baaa3c07a0cb378f9fb07de51f691533e5bfb
rafael-aero <rafael@rafael.aero>
2021-06-14 14:56:55 +0200

    reduce allocations and add benchmark

--- CommandScore.Test/Test.cs
+++ CommandScore.Test/Test.cs
@@ -1,85 +1,105 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace CommandScore.Test
 {
     class Program
     {
+        private static bool anyFailed = false;
+        private static void Assert(bool condition, string message = null)
+        {
+#if DEBUG
+            Debug.Assert(condition, message);
+#endif
+            anyFailed |= !condition;
+        }
+
         static void Main(string[] args)
         {
+            int N = 1000000;
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < N; i++)
+            {
+                if (i % 1000 == 0)
+                {
+                    Console.WriteLine($"At {i:n0} of {N:n0} \t {30*1000 / sw.Elapsed.TotalSeconds:n0} calls/s");
+                    sw.Restart();
+                }
 
-            const double TOLERANCE = 0.001;
-            
-            Debug.Assert(CommandScore.Score("Hello", "Hello") > CommandScore.Score("Hello", "hello"));
+                const float TOLERANCE = 0.00001f;
 
-            // should match exact strings exactly
-            Debug.Assert(CommandScore.Score("hello", "hello").Equals(1), "should match exact strings exactly");
+                Assert(CommandScore.Score("Hello", "Hello") > CommandScore.Score("Hello", "hello"));
 
-            // should prefer case-sensitive matches
-            Debug.Assert(CommandScore.Score("Hello", "Hello") > CommandScore.Score("Hello", "hello"), "should prefer case-sensitive matches");
+                // should match exact strings exactly
+                Assert(CommandScore.Score("hello", "hello").Equals(1), "should match exact strings exactly");
 
-            // should mark down prefixes
-            Debug.Assert(CommandScore.Score("hello", "hello") > CommandScore.Score("hello", "he"), "should mark down prefixes");
+                // should prefer case-sensitive matches
+                Assert(CommandScore.Score("Hello", "Hello") > CommandScore.Score("Hello", "hello"), "should prefer case-sensitive matches");
 
-            // should score all prefixes the same
-            Debug.Assert(Math.Abs(CommandScore.Score("help", "he") - CommandScore.Score("hello", "he")) < TOLERANCE, "should score all prefixes the same");
+                // should mark down prefixes
+                Assert(CommandScore.Score("hello", "hello") > CommandScore.Score("hello", "he"), "should mark down prefixes");
 
-
-            // should mark down word jumps
-            Debug.Assert(CommandScore.Score("hello world", "hello") > CommandScore.Score("hello world", "hewo"), "should mark down word jumps");
-
-
-            // should score similar word jumps the same
-            Debug.Assert(Math.Abs(CommandScore.Score("hello world", "hewo") - CommandScore.Score("hey world", "hewo")) < TOLERANCE, "should score similar word jumps the same");
-
-
-            // should penalize long word jumps
-            Debug.Assert(CommandScore.Score("hello world", "hewo") > CommandScore.Score("hello kind world", "hewo"), "should penalize long word jumps");
+                // should score all prefixes the same
+                Assert(Math.Abs(CommandScore.Score("help", "he") - CommandScore.Score("hello", "he")) < TOLERANCE, "should score all prefixes the same");
 
 
-            // should match missing characters
-            Debug.Assert(CommandScore.Score("hello", "hl") > 0, "should match missing characters");
+                // should mark down word jumps
+                Assert(CommandScore.Score("hello world", "hello") > CommandScore.Score("hello world", "hewo"), "should mark down word jumps");
 
 
-            // should penalize more for more missing characters
-            Debug.Assert(CommandScore.Score("hello", "hllo") > CommandScore.Score("hello", "hlo"), "should penalize more for more missing characters");
+                // should score similar word jumps the same
+                Assert(Math.Abs(CommandScore.Score("hello world", "hewo") - CommandScore.Score("hey world", "hewo")) < TOLERANCE, "should score similar word jumps the same");
 
 
-            // should penalize more for missing characters than case
-            Debug.Assert(CommandScore.Score("go to Inbox", "in") > CommandScore.Score("go to Unversity/Societies/CUE/info@cue.org.uk", "in"), "should penalize more for missing characters than case");
+                // should penalize long word jumps
+                Assert(CommandScore.Score("hello world", "hewo") > CommandScore.Score("hello kind world", "hewo"), "should penalize long word jumps");
 
 
-            // should match transpotisions
-            Debug.Assert(CommandScore.Score("hello", "hle") > 0, "should match transpotisions");
+                // should match missing characters
+                Assert(CommandScore.Score("hello", "hl") > 0, "should match missing characters");
 
 
-            // should not match with a trailing letter
-            Debug.Assert(Math.Abs(CommandScore.Score("ss", "sss") - 0.1) < TOLERANCE, "should not match with a trailing letter");
+                // should penalize more for more missing characters
+                Assert(CommandScore.Score("hello", "hllo") > CommandScore.Score("hello", "hlo"), "should penalize more for more missing characters");
 
 
-            // should match long jumps
-            Debug.Assert(CommandScore.Score("go to @QuickFix", "fix") > 0,                                            "should match long jumps");
-            Debug.Assert(CommandScore.Score("go to Quick Fix", "fix") > CommandScore.Score("go to @QuickFix", "fix"), "should match long jumps");
+                // should penalize more for missing characters than case
+                Assert(CommandScore.Score("go to Inbox", "in") > CommandScore.Score("go to Unversity/Societies/CUE/info@cue.org.uk", "in"), "should penalize more for missing characters than case");
 
 
-            // should work well with the presence of an m-dash
-            Debug.Assert(CommandScore.Score("no go — Windows", "windows") > 0, "should work well with the presence of an m-dash");
+                // should match transpotisions
+                Assert(CommandScore.Score("hello", "hle") > 0, "should match transpotisions");
 
 
-            // should be robust to duplicated letters
-            Debug.Assert(Math.Abs(CommandScore.Score("talent", "tall") - 0.099) < TOLERANCE, "should be robust to duplicated letters");
+                // should not match with a trailing letter
+                Assert(Math.Abs(CommandScore.Score("ss", "sss") - 0.1) < TOLERANCE, "should not match with a trailing letter");
 
 
-            // should not allow letter insertion
-            Debug.Assert(CommandScore.Score("talent", "tadlent") == 0, "should not allow letter insertion");
+                // should match long jumps
+                Assert(CommandScore.Score("go to @QuickFix", "fix") > 0, "should match long jumps");
+                Assert(CommandScore.Score("go to Quick Fix", "fix") > CommandScore.Score("go to @QuickFix", "fix"), "should match long jumps");
 
 
-            // should match - with " " characters
-            Debug.Assert(Math.Abs(CommandScore.Score("Auto-Advance", "Auto Advance") - 0.9999) < TOLERANCE, "should match - with \" \" characters");
+                // should work well with the presence of an m-dash
+                Assert(CommandScore.Score("no go — Windows", "windows") > 0, "should work well with the presence of an m-dash");
 
 
-            // should score long strings quickly
-            Debug.Assert(Math.Abs(CommandScore.Score("go to this is a really long label that is really longthis is a really long label that is really longthis is a really long label that is really longthis is a really long label that is really long", "this is a") - 0.891) < TOLERANCE, "should score long strings quickly");
+                // should be robust to duplicated letters
+                Assert(Math.Abs(CommandScore.Score("talent", "tall") - 0.099) < TOLERANCE, "should be robust to duplicated letters");
+
+
+                // should not allow letter insertion
+                Assert(CommandScore.Score("talent", "tadlent") == 0, "should not allow letter insertion");
+
+
+                // should match - with " " characters
+                Assert(Math.Abs(CommandScore.Score("Auto-Advance", "Auto Advance") - 0.9999) < TOLERANCE, "should match - with \" \" characters");
+
+
+                // should score long strings quickly
+                Assert(Math.Abs(CommandScore.Score("go to this is a really long label that is really longthis is a really long label that is really longthis is a really long label that is really longthis is a really long label that is really long", "this is a") - 0.891) < TOLERANCE, "should score long strings quickly");
+            }
         }
     }
 }
--- CommandScore/CommandScore.cs
+++ CommandScore/CommandScore.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.ObjectPool;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace CommandScore
@@ -11,23 +15,23 @@ namespace CommandScore
         //
         // The best case, this character is a match, and either this is the start
         // of the string, or the previous character was also a match.
-        private const double SCORE_CONTINUE_MATCH = 1.0f;
+        private const float SCORE_CONTINUE_MATCH = 1.0f;
 
         // A new match at the start of a word scores better than a new match
         // elsewhere as it's more likely that the user will type the starts
         // of fragments.
         // NOTE: We score word jumps between spaces slightly higher than slashes, brackets
         // hyphens, etc.
-        private const double SCORE_SPACE_WORD_JUMP     = 0.9f;
-        private const double SCORE_NON_SPACE_WORD_JUMP = 0.8f;
+        private const float SCORE_SPACE_WORD_JUMP     = 0.9f;
+        private const float SCORE_NON_SPACE_WORD_JUMP = 0.8f;
 
         // Any other match isn't ideal, but we include it for completeness.
-        private const double SCORE_CHARACTER_JUMP = 0.3f;
+        private const float SCORE_CHARACTER_JUMP = 0.3f;
 
         // If the user transposed two letters, it should be signficantly penalized.
         //
         // i.e. "ouch" is more likely than "curtain" when "uc" is typed.
-        private const double SCORE_TRANSPOSITION = 0.1f;
+        private const float SCORE_TRANSPOSITION = 0.1f;
 
         // The goodness of a match should decay slightly with each missing
         // character.
@@ -36,7 +40,7 @@ namespace CommandScore
         //
         // This will not change the order of suggestions based on SCORE_* until
         // 100 characters are inserted between matches.
-        private const double PENALTY_SKIPPED = 0.999f;
+        private const float PENALTY_SKIPPED = 0.999f;
 
         // The goodness of an exact-case match should be higher than a
         // case-insensitive match by a small amount.
@@ -45,10 +49,10 @@ namespace CommandScore
         //
         // This will not change the order of suggestions based on SCORE_* until
         // 1000 characters are inserted between matches.
-        private const double PENALTY_CASE_MISMATCH = 0.9999f;
+        private const float PENALTY_CASE_MISMATCH = 0.9999f;
 
         // Match higher for letters closer to the beginning of the word
-        private const double PENALTY_DISTANCE_FROM_START = 0.9f;
+        private const float PENALTY_DISTANCE_FROM_START = 0.9f;
 
         // If the word has more characters than the user typed, it should
         // be penalised slightly.
@@ -59,19 +63,43 @@ namespace CommandScore
         // ordering (like alphabetical) that it makes sense to rely on when
         // there are many prefix matches, so we don't make the penalty increase
         // with the number of tokens.
-        private const double PENALTY_NOT_COMPLETE = 0.99f;
+        private const float PENALTY_NOT_COMPLETE = 0.99f;
 
-        private static readonly Regex IS_GAP_REGEXP      = new Regex("/[\\\\/_+.#\"@\\[\\(\\{&]/");
-        private static readonly Regex COUNT_GAPS_REGEXP  = new Regex("/[\\\\/_+.#\"@\\[\\(\\{&]/g");
-        private static readonly Regex IS_SPACE_REGEXP    = new Regex("/[\\s-]/");
-        private static readonly Regex COUNT_SPACE_REGEXP = new Regex("/[\\s-]/g");
 
-        private static double CommandScoreInner(string item, string abbreviation, string lowerString, string lowerAbbreviation, int stringIndex, int abbreviationIndex, Dictionary<(int, int), double> memoizedResults)
+        private static readonly HashSet<char> IsGap = new HashSet<char>(new[] { '\\', '/', '_', '+', '.', '#', '"', '@', '[', '(', '{', '&' });
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int CountSpaces(ReadOnlySpan<char> text)
         {
-
-            if (abbreviationIndex == abbreviation.Length)
+            int count = 0;
+            foreach (var s in text)
             {
-                if (stringIndex == item.Length)
+                if (char.IsWhiteSpace(s)) count++;
+            }
+            return count;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int CountGaps(ReadOnlySpan<char> text)
+        {
+            int count = 0;
+            foreach (var s in text)
+            {
+                if (IsGap.Contains(s)) count++;
+            }
+            return count;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static long MakeKey(int left, int right)
+        {
+            return (long)left << 32 | (long)(uint)right;
+        }
+        private static float CommandScoreInner(string item, string candidate, string lowerItem, string lowerCandidate, int itemIndex, int candidateIndex, Dictionary<long, float> memoizedResults)
+        {
+            if (candidateIndex == candidate.Length)
+            {
+                if (itemIndex == item.Length)
                 {
                     return SCORE_CONTINUE_MATCH;
 
@@ -79,59 +107,58 @@ namespace CommandScore
                 return PENALTY_NOT_COMPLETE;
             }
 
-            var memoizeKey = (stringIndex, abbreviationIndex);
+            var memoizeKey = MakeKey(itemIndex, candidateIndex);
+
             if (memoizedResults.ContainsKey(memoizeKey))
             {
                 return memoizedResults[memoizeKey];
             }
 
-            var abbreviationChar = lowerAbbreviation[abbreviationIndex];
-            var index = lowerString.IndexOf(abbreviationChar, stringIndex);
-            double highScore = 0f;
+            var candidateChar = lowerCandidate[candidateIndex];
+            var index = lowerItem.IndexOf(candidateChar, itemIndex);
+            float highScore = 0f;
 
-            double score;
-            double transposedScore;
-            Match wordBreaks;
-            Match spaceBreaks;
+            float score;
+            float transposedScore;
 
             while (index >= 0)
             {
 
-                score = CommandScoreInner(item, abbreviation, lowerString, lowerAbbreviation, index + 1, abbreviationIndex + 1, memoizedResults);
+                score = CommandScoreInner(item, candidate, lowerItem, lowerCandidate, index + 1, candidateIndex + 1, memoizedResults);
                 if (score > highScore)
                 {
-                    if (index == stringIndex)
+                    if (index == itemIndex)
                     {
                         score *= SCORE_CONTINUE_MATCH;
                     }
-                    else if (IS_GAP_REGEXP.IsMatch(item[index - 1].ToString()))
+                    else if (IsGap.Contains(item[index - 1]))
                     {
                         score *= SCORE_NON_SPACE_WORD_JUMP;
-                        wordBreaks = COUNT_GAPS_REGEXP.Match(item.Substring(stringIndex, index - 1));
-                        if (wordBreaks.Success && stringIndex > 0)
+                        var countGaps = CountGaps(item.AsSpan(itemIndex, index - 1 - itemIndex));
+                        if (countGaps > 0 && itemIndex > 0)
                         {
-                            score *= Math.Pow(PENALTY_SKIPPED, wordBreaks.Length);
+                            score *= MathF.Pow(PENALTY_SKIPPED, countGaps);
                         }
                     }
-                    else if (IS_SPACE_REGEXP.IsMatch(item[index - 1].ToString()))
+                    else if (char.IsWhiteSpace(item[index - 1]))
                     {
                         score *= SCORE_SPACE_WORD_JUMP;
-                        spaceBreaks = COUNT_SPACE_REGEXP.Match(item.Substring(stringIndex, index - 1));
-                        if (spaceBreaks.Success && stringIndex > 0)
+                        var spaceBreaks = CountSpaces(item.AsSpan(itemIndex, index - 1 - itemIndex));
+                        if (spaceBreaks>0 && itemIndex > 0)
                         {
-                            score *= Math.Pow(PENALTY_SKIPPED, spaceBreaks.Length);
+                            score *= MathF.Pow(PENALTY_SKIPPED, spaceBreaks);
                         }
                     }
                     else
                     {
                         score *= SCORE_CHARACTER_JUMP;
-                        if (stringIndex > 0)
+                        if (itemIndex > 0)
                         {
-                            score *= Math.Pow(PENALTY_SKIPPED, index - stringIndex);
+                            score *= MathF.Pow(PENALTY_SKIPPED, index - itemIndex);
                         }
                     }
 
-                    if (item[index] != abbreviation[abbreviationIndex])
+                    if (item[index] != candidate[candidateIndex])
                     {
                         score *= PENALTY_CASE_MISMATCH;
                     }
@@ -139,12 +166,12 @@ namespace CommandScore
                 }
 
                 if (score < SCORE_TRANSPOSITION
-                 && (lowerString.CharAt(index - 1) == lowerAbbreviation.CharAt(abbreviationIndex + 1)
-                  || lowerAbbreviation[abbreviationIndex + 1] == lowerAbbreviation[abbreviationIndex] // allow duplicate letters. Ref #7428
-                  && lowerString.CharAt(index - 1) != lowerAbbreviation.CharAt(abbreviationIndex)))
+                 && (CharMatch(lowerItem, index - 1, lowerCandidate, candidateIndex + 1)
+                  || lowerCandidate[candidateIndex + 1] == lowerCandidate[candidateIndex] // allow duplicate letters. Ref #7428
+                  && CharMismatch(lowerItem, index - 1, lowerCandidate, candidateIndex)))
                 {
 
-                    transposedScore = CommandScoreInner(item, abbreviation, lowerString, lowerAbbreviation, index + 1, abbreviationIndex + 2, memoizedResults);
+                    transposedScore = CommandScoreInner(item, candidate, lowerItem, lowerCandidate, index + 1, candidateIndex + 2, memoizedResults);
 
                     if (transposedScore * SCORE_TRANSPOSITION > score)
                     {
@@ -157,32 +184,70 @@ namespace CommandScore
                     highScore = score;
                 }
 
-                index = lowerString.IndexOf(abbreviationChar, index + 1);
+                index = lowerItem.IndexOf(candidateChar, index + 1);
             }
 
             memoizedResults[memoizeKey] = highScore;
             return highScore;
         }
 
-        private static string CharAt(this string term, int index)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool CharMatch(string left, int indexLeft, string right, int indexRight)
         {
-            if (term.Length >= index || index < 0) return "";
-            return term[index].ToString();
+            if (indexLeft < 0 || indexLeft > left.Length) return false;
+            if (indexRight < 0 || indexRight > right.Length) return false;
+            return left[indexLeft] == right[indexRight];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool CharMismatch(string left, int indexLeft, string right, int indexRight)
+        {
+            if (indexLeft < 0 || indexLeft > left.Length) return false;
+            if (indexRight < 0 || indexRight > right.Length) return false;
+            return left[indexLeft] != right[indexRight];
         }
 
         private static string FormatInput(string item)
         {
+            var culture = CultureInfo.CurrentUICulture;
+            bool changed = false;
+            var sb = _poolSB.Get();
+            foreach(var c in item)
+            {
+                if (char.IsWhiteSpace(c) || c == '-')
+                {
+                    sb.Append(' ');
+                    if(c != ' ')
+                    {
+                        changed = true;
+                    }
+                }
+                else
+                {
+                    var newC = char.ToLower(c, culture);
+                    sb.Append(newC);
+                    if (c != newC) changed = true;
+                }
+            }
+            
+            if (!changed) return item;
+
             // convert all valid space characters to space so they match each other
-            return COUNT_SPACE_REGEXP.Replace(item.ToLower(), " ");
+            var final = sb.ToString();
+            _poolSB.Return(sb);
+            return final;
         }
 
-        public static double Score(string item, string abbreviation)
+        
+        private static ObjectPool<Dictionary<long, float>> _pool = ObjectPool.Create(new DefaultPooledObjectPolicy<Dictionary<long, float>>());
+        private static ObjectPool<StringBuilder> _poolSB = ObjectPool.Create(new StringBuilderPooledObjectPolicy() { MaximumRetainedCapacity = 2048, InitialCapacity = 64 });
+        public static float Score(string item, string candidate)
         {
-            /* NOTE:
-             * in the original, we used to do the lower-casing on each recursive call, but this meant that toLowerCase()
-             * was the dominating cost in the algorithm, passing both is a little ugly, but considerably faster.
-             */
-            return CommandScoreInner(item, abbreviation, FormatInput(item), FormatInput(abbreviation), 0, 0, new Dictionary<(int, int), double>());
+            
+            var dict = _pool.Get();
+            var score = CommandScoreInner(item, candidate, FormatInput(item), FormatInput(candidate), 0, 0, dict);
+            _pool.Return(dict);
+            return score;
         }
     }
 }
